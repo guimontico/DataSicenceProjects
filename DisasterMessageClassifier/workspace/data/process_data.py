01221c887ff4455e9fecd201eb456bbf55b712ec
Guilherme Montico <38353832+guimontico@users.noreply.github.com>
2020-05-04 12:07:39 -0300

    Updating Project
    Project develop with Udacity Data Science Nanodeegre

---
+++ DisasterMessageClassifier/workspace/data/process_data.py
@@ -0,0 +1,90 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    
+    df = messages.merge(categories, on='id')
+    
+    return df
+
+
+def clean_data(df):
+    '''
+    Function to ake a loaded dataset from load_data and returns a cleaned dataset
+    
+    Input:
+        df: Data frame created from `load_data`
+    Returns:
+        df: Cleaned data frame.
+    '''
+    #split the categories into columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    #getting column names for categories
+    categories_row = categories.loc[0]
+    category_colnames = categories_row.apply(lambda x: x[:-2])
+    
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set the values on the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+    
+        # convert columns to numeric
+        categories[column] = categories[column].astype(int)
+    
+    df = df.drop(['categories'], axis=1)
+    df = pd.concat([df,categories], axis=1)
+    #using duplicated function to drop rows
+    df = df[df.duplicated() == False]
+    
+    return df
+
+
+def save_data(df, database_filename):
+    """
+        Function to save processed dataframe into sqlite database
+    Input: 
+        df: The preprocessed dataframe
+        database_filename: name of the database
+    Returns: 
+        None
+    """
+
+    # save data into a sqlite database
+    engine = create_engine('sqlite:///data/DisasterResponse.db')
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ DisasterMessageClassifier/workspace/models/train_classifier.py
@@ -0,0 +1,151 @@
+import sys
+import pandas as pd
+import numpy as np
+import pickle
+import re
+import nltk
+from sqlalchemy import create_engine
+from sklearn.utils.multiclass import type_of_target
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import accuracy_score
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sqlalchemy import create_engine
+
+nltk.download('stopwords')
+
+def load_data(database_filepath):
+    """
+        Load data from the sqlite database. 
+    Input: 
+        database_filepath: the path of the database file
+    Returns: 
+        X: messages 
+        Y: categories
+        category_names (List)
+    """
+    
+    # load data from database
+    engine = create_engine('sqlite:///data/DisasterResponse.db')
+    df = pd.read_sql_table('DisasterResponse', engine)
+    df = df[~(df == 2).any(axis=1)]
+    df = df.drop(columns=['child_alone'])
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns
+    return X, Y, category_names
+
+
+def tokenize(text):
+    """
+    Normalizes, tokenizes and lemms text
+    
+    Input:
+        text: tweet from disasters
+    Returns:
+        clean_tokens: tokenized message
+    """
+    
+    # adjusting text
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    
+    # Create tokens 
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords
+    tokens = [t for t in tokens if t not in stopwords.words("english")]
+    
+    # Lemmatise words
+    clean_tokens = [WordNetLemmatizer().lemmatize(t) for t in tokens]
+
+    return clean_tokens
+
+
+def build_model():
+    """
+    Creates machine learning pipeline
+    
+    Returns:
+        pipeline: Machine Learning pipeline
+    """
+    
+    logit_reg = LogisticRegression(random_state=42)
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(logit_reg, n_jobs=-1))
+    ])
+    
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """   
+    Input:
+        model: Model trained
+        X_test: Dataframe, validation data for model
+        Y_test: Dataframe, actual labels for the test data in X
+        category_names: categories to be evaluated
+    Returns:
+        None: Prints out report
+    """
+    y_pred = model.predict(X_test)
+
+    print (classification_report(Y_test, y_pred, target_names=category_names))
+    print (accuracy_score(Y_test, y_pred))
+
+
+def save_model(model, model_filepath):
+    """
+    Saves model as a pickle file to model_filepath
+    
+    Input:
+        model: model to be pickled
+        model_filepath: filepath where model will be saved
+    Returns:
+        None: Pickle file will be created at model_path
+    """
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))    
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
